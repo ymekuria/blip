095d130a68100630098aa12667878923f1db6c71
Yoni <ymekuria@gmail.com>
2017-06-14 16:00:02 -0700

    added boilerplate for ReviewSavedScreen

---
+++ src/screens/ReviewSavedScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ReviewSavedScreen extends Component {
+  render() {
+    return (
+      <View> 
+        <Text>ReviewSavedScreen</Text>
+        <Text>ReviewSavedScreen</Text>
+        <Text>ReviewSavedScreen</Text>
+        <Text>ReviewSavedScreen</Text> 
+      </View>
+    );  
+  }
+}
+
+export default ReviewSavedScreen;
+
