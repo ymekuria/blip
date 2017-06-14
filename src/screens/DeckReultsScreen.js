a4ecfde86944cff5c4652e813b254a2503c1fe08
Yoni <ymekuria@gmail.com>
2017-06-14 15:57:34 -0700

    added boilerplate for DeckResultsScreen

---
+++ src/screens/DeckReultsScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class DeckResultsScreen extends Component {
+  render() {
+    return (
+      <View> 
+        <Text>DeckResultsScreen</Text>
+        <Text>DeckResultsScreen</Text>
+        <Text>DeckResultsScreen</Text>
+        <Text>DeckResultsScreen</Text> 
+      </View>
+    );  
+  }
+}
+
+export default DeckResultsScreen;
+
