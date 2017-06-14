f3341a31f1d11673ed9ec48abea021c78fc2b70b
Yoni <ymekuria@gmail.com>
2017-06-14 15:56:03 -0700

    added boilerplate for MapSearchScreen

---
+++ src/screens/MapSearchScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class MapSearchScreen extends Component {
+  render() {
+    return (
+      <View> 
+        <Text>MapSearchScreen</Text>
+        <Text>MapSearchScreen</Text>
+        <Text>MapSearchScreen</Text>
+        <Text>MapSearchScreen</Text> 
+      </View>
+    );  
+  }
+}
+
+export default MapSearchScreen;
+
