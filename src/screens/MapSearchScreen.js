fe923cfd333e5d1c35479bed402058641e90ce5e
Yoni <ymekuria@gmail.com>
2017-06-16 16:24:35 -0700

    setting the mapSearchScreen initial MapView to the current position of the device using the app

--- src/screens/MapSearchScreen.js
+++ src/screens/MapSearchScreen.js
@@ -3,16 +3,38 @@ import { View, Text } from 'react-native';
 import { MapView } from 'expo'
 
 class MapSearchScreen extends Component {
+  // setting initial map region to San Francisco
+  state = {
+    region: {
+      latitude: 37.3861,
+      longitude: -122.0839,
+      latitudeDelta: 0.05,
+      longitudeDelta: 0.1,
+    }
+  }
+  componentDidMount() {
+    // getting the current location of the device to show initial map region
+    navigator.geolocation.getCurrentPosition(position => {
+        // const { latitude, longitude } = position.coords;
+        const region = {
+          latitude: position.cords.latitude,
+          longitude: position.cords.longitude,
+          latitudeDelta: .05,
+          longitudeDelta: .1
+        };
+
+        this.setState({ region });
+      },
+      (error) => console.log(error),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    ); 
+
+  }
   render() {
     return (
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        initialRegion={this.state.region}
       />
     );
   }
