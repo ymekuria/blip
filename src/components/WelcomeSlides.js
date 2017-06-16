74eabcae8e77a55ca45ab0404eaf890ed139b8d6
Yoni <ymekuria@gmail.com>
2017-06-15 21:59:50 -0700

    split WelcomeSlides into a seperate reusable component

---
+++ src/components/WelcomeSlides.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, ScrollView, Text, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class WelcomeSlides extends Component {
+  renderSlides() {
+    console.log(this.props)
+    return this.props.slideData.map(slide => {
+      return (
+        <View
+          style={[styles.slideStyle, { backgroundColor: slide.color }]}
+          key={slide.text}
+        >
+          <Text style={styles.textStyle}>{slide.text}</Text>
+        </View>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView
+        horizontal
+        pagingEnabled
+        style={{ flex: 1 }}
+      > 
+        {this.renderSlides()}
+      </ScrollView>
+    );  
+  }
+}
+
+const styles = {
+  slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH
+  },
+
+  textStyle: {
+    fontSize: 30,
+    textAlign: 'center'
+    
+  }
+}
+
+export default WelcomeSlides;
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,54 +1,18 @@
 import React, { Component } from 'react';
 import { ScrollView, View, Text, Dimensions } from 'react-native';
+import WelcomeSlides from '../components/WelcomeSlides';
 
-const slideText = [
+const slideData = [
   { text: 'Lorem ipsum dolor sit amet', color: '#009688' },
   { text: 'Accusamus complectitur sit cu', color: '#009688' },
   { text: 'Cetero vituperatoribus no quo', color: '#009688' }
 ];
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
-
 class WelcomeScreen extends Component {
-  renderSlides() {
-    return slideText.map(slide => {
-      return (
-        <View
-          style={[styles.slideStyle, { backgroundColor: slide.color }]}
-          key={slide.text}
-        >
-          <Text style={styles.textStyle}>{slide.text}</Text>
-        </View>
-      );
-    });
-  }
-
   render() {
-
     return (
-      <ScrollView
-        horizontal
-        pagingEnabled
-        style={{ flex: 1 }}
-      > 
-        {this.renderSlides()}
-      </ScrollView>
+      <WelcomeSlides slideData={slideData}/>
     );  
-  }
-}
-
-const styles = {
-  slideStyle: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: SCREEN_WIDTH
-  },
-
-  textStyle: {
-    fontSize: 30,
-    textAlign: 'center'
-    
   }
 }
 
