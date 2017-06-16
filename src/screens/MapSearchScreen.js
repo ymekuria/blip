ebc070e03e6214b5f2d13871e65a99244280bf77
Yoni <ymekuria@gmail.com>
2017-06-16 13:26:19 -0700

    added a Mapview component to the MapSearchScreen

--- src/screens/MapSearchScreen.js
+++ src/screens/MapSearchScreen.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { MapView } from 'expo'
 
 class MapSearchScreen extends Component {
   render() {
     return (
-      <View> 
-        <Text>MapSearchScreen</Text>
-        <Text>MapSearchScreen</Text>
-        <Text>MapSearchScreen</Text>
-        <Text>MapSearchScreen</Text> 
-      </View>
-    );  
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    );
   }
 }
 
