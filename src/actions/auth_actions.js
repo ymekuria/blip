eb81ace52503850373347b8546b942dfcdd1c8f6
Yoni <ymekuria@gmail.com>
2017-06-23 14:22:14 -0700

    updated git ignore

---
+++ src/actions/auth_actions.js
@@ -0,0 +1,7 @@
+import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
+
+// using redux think for async actions 
+export const fbLogin = async dispatch => {
+	// let login = await Facebook.logInWith
+}
