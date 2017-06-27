712c8dd96605757adddd317077d640bc16086567
Yoni <ymekuria@gmail.com>
2017-06-27 15:32:48 -0700

    storing Facebook auth token in AsyncStorage on the users device

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -6,10 +6,22 @@ import {
 	FACEBOOK_LOGIN_FAIL
 	} from './types';
 
-// using redux think for async actions 
-export const fbLogin = () => async dispatch => {
+
+// using redux thunk for async actions 
+export const facebookLogin = () => async dispatch => {
+	// first checking if a fb session token is stored on the users device before authenticating 
+	let token = AsyncStorage.getItem('fb_token');
+
+	if (token) {
+		return dispatch({ type: 'FACEBOOK_LOGIN_SUCCESS', payload: token });
+	} else {
+
+		performFbLogin(dispatch);
+	}
+}
+const performFbLogin = async dispatch => {
 	// will prompt the user to login via a FB modal and return an object with info about the login ie tokens succes/fail
-	let login = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
+	let { token, type } = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
 		permissions: ['public_profile']
 	});
 	console.log('login', login);
