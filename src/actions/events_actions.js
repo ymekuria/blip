ac44e1f5561938c9a19553d9829820690068340c
Yoni <ymekuria@gmail.com>
2017-07-03 11:28:27 -0700

    connected events_action creator to songkick api

--- src/actions/events_actions.js
+++ src/actions/events_actions.js
@@ -5,7 +5,7 @@ import { SONGKICK_API_KEY } from '../../env/config';
 const ROOT_URL = 'http://api.songkick.com/api/3.0/';
 
 export const fetchEvents = () => async (dispatch) => {
-	let events = await axios.get('http://api.songkick.com/api/3.0/events.json?apikey=VzzzDni1eBpqwJzY&artist_name=tycho&location=geo:37.3861,-122.0839');
+	let { data: { resultsPage: { results: {event: events } } } } = await axios.get('http://api.songkick.com/api/3.0/events.json?apikey=VzzzDni1eBpqwJzY&location=geo:37.3861,-122.0839');
 
-	console.log(events);
+	console.log('events', events);
 }
