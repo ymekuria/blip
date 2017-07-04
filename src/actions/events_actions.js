53f05ea4b0862681c4ec6cad800c159d9e5cf50a
Yoni <ymekuria@gmail.com>
2017-07-03 17:48:47 -0700

    created a helper function to build api url

--- src/actions/events_actions.js
+++ src/actions/events_actions.js
@@ -1,11 +1,24 @@
 import axios from 'axios';
+import qs from 'qs';
+import moment from 'moment';
 import { SONGKICK_API_KEY } from '../../env/config';
 
 
-const ROOT_URL = 'http://api.songkick.com/api/3.0/';
+const ROOT_URL = 'http://api.songkick.com/api/3.0/events.json?';
 
-export const fetchEvents = () => async (dispatch) => {
-	let { data: { resultsPage: { results: {event: events } } } } = await axios.get('http://api.songkick.com/api/3.0/events.json?apikey=VzzzDni1eBpqwJzY&location=geo:37.3861,-122.0839');
+const buildUrl = ({ lattitude, longitude }) => {
+	const currentDate = moment().format('YYYY-MM-DD');
+	const eventsQueryParams = {
+		apikey: SONGKICK_API_KEY,
+		location: `geo:${lattitude},${longitude}`,
+		min_date: currentDate,
+		max_date: currentDate
+	}
+	return `${ROOT_URL}${qs.stringify(eventsQueryParams, { encode: false })}`;
+}
 
+export const fetchEvents = (location) => async (dispatch) => {
+	const url = buildUrl(lcation);
+	let { data: { resultsPage: { results: {event: events } } } } = await axios.get(url);
 	console.log('events', events);
 }
