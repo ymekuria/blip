22253e001b395d4f1eb3e5a33af01a650a39ef21
Yoni <ymekuria@gmail.com>
2017-07-05 18:12:11 -0700

    added a screen to view all saved event locations on a single map

---
+++ src/screens/ReviewSavedMapScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+
+class ReviewSavedMapScreen extends Component {
+	render() {
+		return (
+			<View>
+			</View>		
+		);	
+	}
+}
+
+export default ReviewSavedMapScreen
--- src/screens/ReviewSavedScreen.js
+++ src/screens/ReviewSavedScreen.js
@@ -6,9 +6,7 @@ import { Card, Button } from 'react-native-elements';
 
 class ReviewSavedScreen extends Component {
   renderSavedEvents() {
-
     return this.props.savedEvents.map(event => {
-      console.log('event', event)
       const { displaName, location, venue, id } = event;
       const initialRegion = { 
         latitude: location.lat, 
@@ -16,39 +14,40 @@ class ReviewSavedScreen extends Component {
         latitudeDelta: .045,
         longitudeDelta: .02
       }
-
-        return (
-          <Card title={displaName} key={id}>
-            <View style={{ height: 200 }}>
-              <MapView
-                scrollEnabled={false}
-                cacheEnabled
-                style={{ flex: 1 }}
-                initialRegion={initialRegion}
-              >
-                <MapView.Marker
-                  coordinate={{ latitude: location.lat, longitude: location.lng }}       
-                />
-              </MapView>
-            </View>
-            <View style={styles.detailWrapper}>
-              <Text>{venue.displayName}</Text>
-              <Text>{location.city.split(',')[0]}</Text>
-            </View>
-            <Button
-              title="Buy Tickets"
-              backgroundColor="#03A9F4"
-              onPress={() => Linking.openURL(event.uri)}
-            />
-          </Card>
-        );
+      // TODO: add custom map markers
+      return (
+        <Card title={displaName} key={id}>
+          <View style={{ height: 200 }}>
+            <MapView
+              scrollEnabled={false}
+              cacheEnabled={Platform.OS === 'android'}
+              style={{ flex: 1 }}
+              initialRegion={initialRegion}
+            >
+              <MapView.Marker
+                coordinate={{ latitude: location.lat, longitude: location.lng }}       
+              />
+            </MapView>
+          </View>
+          <View style={styles.detailWrapper}>
+            <Text>{venue.displayName}</Text>
+            <Text>{location.city.split(',')[0]}</Text>
+          </View>
+          <Button
+            title="Buy Tickets"
+            backgroundColor="#03A9F4"
+            onPress={() => Linking.openURL(venue.uri)}
+          />
+        </Card>
+      );
     });
   }
+
   render() {
-    return (
-      <ScrollView> 
-        {this.renderSavedEvents()} 
-      </ScrollView>
+   return (
+      <View style={{ flex: 1 }}> 
+        {this.renderSavedEvents()}     
+      </View>
     );  
   }
 }
