a9ae63f7445a5977599716457659e1a96d037403
Yoni <ymekuria@gmail.com>
2017-11-14 14:23:35 -0800

    formatted actions with prettier

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -1,40 +1,39 @@
 import { AsyncStorage } from 'react-native';
 import { Facebook } from 'expo';
 import { FACEBOOK_APP_ID } from '../../env/config';
-import { 
-	FACEBOOK_LOGIN_SUCCESS,
-	FACEBOOK_LOGIN_FAIL
-	} from './types';
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from './types';
 
-
-// using redux thunk for async actions 
+// using redux thunk for async actions
 export const facebookLogin = () => async dispatch => {
-	// first checking if a fb session token is stored on the users device before authenticating
-	try {
-		let token = await AsyncStorage.getItem('fb_token');
+  // first checking if a fb session token is stored on the users device before authenticating
+  try {
+    let token = await AsyncStorage.getItem('fb_token');
 
-		if (token) {
-			return dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
-		} else {
-			performFbLogin(dispatch);
-		}
-	} catch(err) {
-		console.log(err);
-	}
-}
+    if (token) {
+      return dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+    } else {
+      performFbLogin(dispatch);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const performFbLogin = async dispatch => {
-	// will prompt the user to login via a FB modal and return an object with info about the login ie tokens succes/fail
-	let { token, type } = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
-		permissions: ['public_profile']
-	});
+  // will prompt the user to login via a FB modal and return an object with info about the login ie tokens succes/fail
+  let {
+    token,
+    type
+  } = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
+    permissions: ['public_profile']
+  });
 
-	if (type === 'cancel' ) {
-		return dispatch({ type: FACEBOOK_LOGIN_FAIL });
-	} 
-	
-	// saving the fb_token on the users device for future authentication
-	await AsyncStorage.setItem('fb_token', token);
+  if (type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
 
-	return dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
-}
+  // saving the fb_token on the users device for future authentication
+  await AsyncStorage.setItem('fb_token', token);
+
+  return dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+};
--- src/actions/events_actions.js
+++ src/actions/events_actions.js
@@ -4,33 +4,35 @@ import moment from 'moment';
 import { SONGKICK_API_KEY } from '../../env/config';
 import { FETCH_EVENTS, SAVE_EVENT, DELETE_SAVED_EVENTS } from './types';
 
-
 const ROOT_URL = 'http://api.songkick.com/api/3.0/events.json?';
 
 const buildUrl = ({ latitude, longitude }) => {
-	const currentDate = moment().format('YYYY-MM-DD');
-	const eventsQueryParams = {
-		apikey: SONGKICK_API_KEY,
-		location: `geo:${latitude},${longitude}`,
-		min_date: currentDate,
-		max_date: currentDate
-	}
-	return `${ROOT_URL}${qs.stringify(eventsQueryParams, { encode: false })}`;
-}
+  const currentDate = moment().format('YYYY-MM-DD');
+  const eventsQueryParams = {
+    apikey: SONGKICK_API_KEY,
+    location: `geo:${latitude},${longitude}`,
+    min_date: currentDate,
+    max_date: currentDate
+  };
+  return `${ROOT_URL}${qs.stringify(eventsQueryParams, { encode: false })}`;
+};
 
-export const fetchEvents = (location, callBack) => async (dispatch) => {
-	const url = buildUrl(location);
-	console.log('url ', url);
-	let { data: { resultsPage: { results: {event: events } } } } = await axios.get(url);
-	
-	dispatch({ type: FETCH_EVENTS , payload: events });
-	callBack();
-}
+// passing a callback that has access to navigation for navigating after api call
+export const fetchEvents = (location, callBack) => async dispatch => {
+  const url = buildUrl(location);
 
-export const saveEvent = (event) => {
-	return { type: SAVE_EVENT, payload: event }
-}
+  let {
+    data: { resultsPage: { results: { event: events } } }
+  } = await axios.get(url);
+
+  dispatch({ type: FETCH_EVENTS, payload: events });
+  callBack();
+};
+
+export const saveEvent = event => {
+  return { type: SAVE_EVENT, payload: event };
+};
 
 export const deleteSavedEvents = () => {
-	return { type: DELETE_SAVED_EVENTS }
-}
+  return { type: DELETE_SAVED_EVENTS };
+};
